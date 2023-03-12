84c8a6b8b7a0b565b17b133a157481835ccf90d6
Prudexxx <105013402+Prudexxx@users.noreply.github.com>
2023-03-12 13:38:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const boxes = Array.from(document.querySelectorAll(".box")); // считываем все элементы аккордеона в массив
 
-boxes.forEach((box) => {
-  box.addEventListener("click", boxHandler); // при нажатии на бокс вызываем ф-ию boxHanlder
+boxes.forEach((label) => {
+  label.addEventListener("click", boxHandler); // при нажатии на бокс вызываем ф-ию boxHanlder
 });
 
 function boxHandler(e) {
   e.preventDefault(); // сбрасываем стандартное поведение
-  let currentBox = e.target.closest(".box"); // определяем текущий бокс
+  let currentBox = e.target.closest(".label"); // определяем текущий бокс
   let currentContent = e.target.nextElementSibling; // находим скрытый контент
   currentBox.classList.toggle("active"); // присваиваем ему активный класс
   if (currentBox.classList.contains("active")) {
